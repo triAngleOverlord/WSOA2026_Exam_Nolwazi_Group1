8ab60fd63ef4cde57b3fff07f8f48749b6339707
triAngleOverlord <2328564@students.wits.ac.za>
2023-11-08 22:56:24 +0200

    Dialogue flows and options work (with conditions as well)

--- WSOA2026_Exam Build_Nolwazi_Group 1/Assets/Dialogue/Test.ink
+++ WSOA2026_Exam Build_Nolwazi_Group 1/Assets/Dialogue/Test.ink
@@ -1,16 +1,16 @@
-VAR choiceOne = true
+VAR choiceOne = true//variable that unity code can influence
 
-->introElder
+->introElder//goes into the introElder knot
 
 ===introElder===
 
 The elder asks where Nolwazi is going?
-    + [{choiceOne: ? | Hello }]
-        {choiceOne: The elder doesn't seem to understand you. ->introElder | The elder slaps his thigh and laughs! ->main }
+    + [{choiceOne:  Hello| ? }]
+        {choiceOne:The elder slaps his thigh and laughs! ->main | The elder doesn't seem to understand you. ->introElder  }
     * You now understand the language
-        ~choiceOne = false
+        ~choiceOne = true
         ->introElder
-->DONE 
+->DONE
 === main ===
 Ah yes Nolwazi, you can now understand me!
 ->DONE
--- WSOA2026_Exam Build_Nolwazi_Group 1/Assets/Scripts/DialogueManager.cs
+++ WSOA2026_Exam Build_Nolwazi_Group 1/Assets/Scripts/DialogueManager.cs
@@ -1,8 +1,10 @@
 using Ink.Runtime;
+using System.Collections;
 using System.Collections.Generic;
 using System.Xml.Serialization;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class DialogueManager : MonoBehaviour
 {
@@ -21,6 +23,7 @@ public class DialogueManager : MonoBehaviour
     [Header("Choices UI")]
     [SerializeField] private GameObject[] choices;
     private TextMeshProUGUI[] choicesText;
+    [SerializeField] private GameObject continueBTN;
 
     private void Awake()
     {
@@ -56,11 +59,11 @@ public class DialogueManager : MonoBehaviour
         {
             return;
         }
-
+        /*
         if (Input.GetKeyDown(KeyCode.Space))
         {
             continueStory();
-        }
+        }*/
     }
 
     public void enterDialogueMode(TextAsset inkJSON)
@@ -73,12 +76,13 @@ public class DialogueManager : MonoBehaviour
         continueStory();
     }
 
-    private void continueStory()
+    public void continueStory()
     {
         
         if (currentStory.canContinue)
         {
             npcDialogue.text = currentStory.Continue();
+            displayChoices();
         }
         else
         {
@@ -98,5 +102,53 @@ public class DialogueManager : MonoBehaviour
     private void displayChoices()
     {
         List<Choice> currentChoices = currentStory.currentChoices;
+
+        if (currentChoices.Count != 0)
+        {
+            continueBTN.SetActive(false);
+            if (currentChoices.Count > choices.Length)//makes sure there is enough UI support
+            {
+                Debug.LogError("More choices were given then the UI can support. Number of choices given: " + currentChoices.Count);
+            }
+
+            int i = 0;
+            foreach (Choice choice in currentChoices)
+            {
+                choices[i].gameObject.SetActive(true);
+                choicesText[i].text = choice.text;
+                Debug.Log(choice.text);
+                i++;
+            }
+
+            for (int j = i; j < choices.Length; j++)//hide unused UI support
+            {
+                choices[j].gameObject.SetActive(false);
+            }
+
+            StartCoroutine(SelectFirstChoice());
+        }
+        else 
+        { 
+            foreach (GameObject btnChoices in choices)
+            {
+                btnChoices.SetActive(false);
+            }
+            continueBTN.SetActive(true);
+        }
+        
+    }
+
+    private IEnumerator SelectFirstChoice()
+    {
+        //event system handles disributing the action of the buttons after waiting 1 frame
+        EventSystem.current.SetSelectedGameObject(null);
+        yield return new WaitForEndOfFrame();
+        EventSystem.current.SetSelectedGameObject(choices[0].gameObject);
+    }
+
+    public void makeChoice(int choiceIndex)
+    {
+        currentStory.ChooseChoiceIndex(choiceIndex);
+        continueStory();
     }
 }
