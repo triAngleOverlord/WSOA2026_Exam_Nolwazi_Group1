bf2ca89be85e7ce2dbc7b8298558aac68bd0bd0a
triAngleOverlord <2328564@students.wits.ac.za>
2023-11-14 11:56:24 +0200

    nigerian puzzle finished player screen finished added sprite assets

--- WSOA2026_Exam Build_Nolwazi_Group 1/Assets/Scripts/PuzzleStuff/nigerianPuzzle.cs
+++ WSOA2026_Exam Build_Nolwazi_Group 1/Assets/Scripts/PuzzleStuff/nigerianPuzzle.cs
@@ -3,12 +3,13 @@ using UnityEngine;
 
 public class nigerianPuzzle : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI inputField;
+    [SerializeField] TMP_InputField[] inputField;
     // Start is called before the first frame update
     
     public void checkNumber()
     {
-        if (inputField.text == "16")
+        
+        if (inputField[0].text == "210" && inputField[1].text == "350" && inputField[2].text == "230" && inputField[3].text == "510")
             Debug.Log("You are correct!");
         else
             Debug.Log("You are incorrect!");
